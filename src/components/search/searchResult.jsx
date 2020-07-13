d1fa67037c0fe27b1bb871c1d8c1762abfe8b6fe
gigamarr <nofacemarr@gmail.com>
2020-07-13 14:37:10 +0400

    inaccessible movie filtering

--- src/components/search/searchResult.jsx
+++ src/components/search/searchResult.jsx
@@ -10,27 +10,35 @@ function SearchResult(props) {
     const { name: title, isTvShow, year, adjaraId } = props.movie
     const [hoverState, setHoverState] = useState(false)
     
+    /* `adjaraId === null` means the movie is not accessible
+        but there are cases where they are accessible regardless of the `adjaraId`,
+        those cases usually involve content taken down due to DMCA strikes.
+    */
     return (
-        <Link to={`/watch/${adjaraId}/${slugify(title)}`} className="search-result-anchor">
-            <div className={calculateClassName(props, hoverState)} 
-                onMouseEnter={() => setHoverState(true)}
-                onMouseLeave={() => setHoverState(false)}
-            >
-                <div>
-                    <p title={title}>{title.length <= 35 ? title : title.slice(0,36) + '...'}</p>
-                </div>
+        <React.Fragment>
+            {adjaraId !== null && (
+                <Link to={`/watch/${adjaraId}/${slugify(title)}`} className="search-result-anchor">
+                    <div className={calculateClassName(props, hoverState)} 
+                        onMouseEnter={() => setHoverState(true)}
+                        onMouseLeave={() => setHoverState(false)}
+                    >
+                        <div>
+                            <p title={title}>{title.length <= 35 ? title : title.slice(0,36) + '...'}</p>
+                        </div>
 
-                <div className="result-meta">
-                    <div className="meta-type">
-                        <p>{isTvShow ? 'TV Show' : 'Movie'}</p>
-                    </div>
+                        <div className="result-meta">
+                            <div className="meta-type">
+                                <p>{isTvShow ? 'TV Show' : 'Movie'}</p>
+                            </div>
 
-                    <div>
-                        <p>({year})</p>
+                            <div>
+                                <p>({year})</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </Link>
+                </Link>
+            )}
+        </React.Fragment>
     )
 }
 
