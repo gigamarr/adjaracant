6169dcdf131d0f34478eb9c6a2a62d3ebebe6c2f
gigamarr <nofacemarr@gmail.com>
2020-08-08 00:38:40 +0400

    changed proxy server address

--- src/services/adjaranetService.js
+++ src/services/adjaranetService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://api.adjaracant.xyz",
     withCredentials: false,
     headers: {
         Accept: 'application/json',
